667b2022dd386dd36956b1b0d596c031aeb7935a
Aryan Ganotra <aryanganotra7@gmail.com>
2019-10-17 18:35:10 +0530

    Update Server-MultiThreading.py

--- Server-MultiThreading.py
+++ Server-MultiThreading.py
@@ -1,3 +1,32 @@
 import socket
 import sys
+
+def read(conn):
+    while True:
+        try:
+            data = connection.recv(1028)
+            print ( 'client:',data)
+        except:
+            continue
+def write(conn,msg):
+    while True:
+        try:
+            conn.sendall((message+'\n').encode('utf-8'))
+        except:
+            continue
+            
+    
+            
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(socket.gethostname())
+server_address = ('192.168.3.113',10000)
+print ("starting up on %s port %s" % server_address)
+sock.bind(server_address)
+sock.listen(1)
+print ("waiting for a connection")
+connection, client_Address = sock.accept()
+print ('connection from', client_Address)
+
+        
+        
+        
